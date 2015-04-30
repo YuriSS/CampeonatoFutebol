5155f2c9022a0ed6c95e4e967833f372ce84cc8d
Yuri Santos <yuri.s.dos.santos@gmail.com>
2015-04-30 16:35:57 -0300

    Update main.c
    Main funcionando

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "champs.h"
+#include "equipes.h"
 
 
 int main() {
@@ -11,59 +11,27 @@ int main() {
 	
 	FILE *f = fopen("texto.txt", "r");
 	if(f == NULL) {
-		printf("veio");
 		return 0;
 	}
 
-	int i=0, codigo_1, codigo_2, gols_1, gols_2, sh;
+	int i=0, codigo_1, codigo_2, gols_1, gols_2;
 	while(fscanf(f, "%s", string) != EOF) {
-		if(i%4==0) {
-			codigo_1 = atoi(string);
-		} else if(i%4==1) {
-			gols_1 = atoi(string);
-		} else if(i%4==2) {
-			codigo_2 = atoi(string);
-		} else if(i%4==3) {
-			gols_2 = atoi(string);
+		if(i > 0) {
+			if(i%4==1) {
+				codigo_1 = atoi(string);
+			} else if(i%4==2) {
+				gols_1 = atoi(string);
+			} else if(i%4==3) {
+				codigo_2 = atoi(string);
+			} else if(i%4==0) {
+				gols_2 = atoi(string);
+				partida(bra, codigo_1, gols_1, codigo_2, gols_2);
+			}
 		}
 		i++;
-		printf("%d\n", gols_1);
-		push(bra, codigo_1);
-		push(bra, codigo_2);
-		partida(bra, codigo_1, gols_1, codigo_2, gols_2);
-		sh = search(bra, codigo_1);
-		ranking(bra, sh);
-		sh = search(bra, codigo_2);
-		ranking(bra, sh);
 	}
 	fclose(f);
 	toString(bra);
-
-	/*
-	int sh;
-	push(bra, 300);
-	push(bra, 301);
-
-	partida(bra, 301, 1, 300, 2);
-	
-	sh = search(bra, 300);
-	ranking(bra, sh);
-	
-	sh = search(bra, 301);
-	ranking(bra, sh);
-	
-	push(bra, 302);
-	push(bra, 303);
-	
-	partida(bra, 302, 2, 303, 0);
-	
-	sh = search(bra, 302);
-	ranking(bra, sh);
-	
-	sh = search(bra, 303);
-	ranking(bra, sh);
-	
-	toString(bra);*/
 
 	return 0;
 }
